25022e32ee0f41ba113e09ea0d86665a263f1378
TremMT <tremariet@gmail.com>
2023-07-15 20:19:47 -0700

    Skillcrush challenge COMPLETE!

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 // Your JS code here
+
+//You’ll write event listeners, event handlers, function expressions, and callback functions to calculate the numbers and return a message.
+
+var subTypeElement = document.querySelector("#subscription");
+var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
+var subType = "basic";
+var subDuration = 1;
+
+subTypeElement.addEventListener("change", function (e) {
+  subType = e.target.value;
+  //console.log(subType);
+  updateSubscriptionDiv();
+});
+
+subDurationElement.addEventListener("change", function (Number) {
+  subDuration = Number.target.value;
+  //console.log(subDuration);
+  updateSubscriptionDiv();
+});
+
+var updateSubscriptionDiv =
+  ("input",
+  function () {
+    var monthlyCost = 5;
+    if (subType === "standard") {
+      monthlyCost = 7;
+    } else if (subType === "premium") {
+      monthlyCost = 10;
+    }
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+  });
